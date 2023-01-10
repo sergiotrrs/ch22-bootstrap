0f2d5ff735f2bd7e710084e89afc29335830e434
Sergio Torres <sergio.torres@idr.org.mx>
2023-01-10 11:34:13 -0600

    ci: modifica función change name

--- practica/hola-nombre.js
+++ practica/hola-nombre.js
@@ -2,5 +2,5 @@ console.log("Práctica Di tu nombre");
 
 function changeName(){
     const name = prompt("Por favor, escribe tu nombre:");
-    document.getElementById("hello").innerHTML = `Hola ${name}`;
+    document.getElementById("hello").inerHTML = `Hola ${name}`;
 }
